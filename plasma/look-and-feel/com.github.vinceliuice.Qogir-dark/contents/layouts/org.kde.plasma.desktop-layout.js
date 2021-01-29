369eb49ae85eec81929af3534e2a92d9b0cb5bdd
mrrbrilliant <mrrbrilliantwork@gmail.com>
2021-01-29 13:52:23 +0700

    removed qogir sddm

--- plasma/look-and-feel/com.github.vinceliuice.Qogir-dark/contents/layouts/org.kde.plasma.desktop-layout.js
+++ plasma/look-and-feel/com.github.vinceliuice.Qogir-dark/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,151 +1,139 @@
 var plasma = getApiVersion(1);
 
 var layout = {
-    "desktops": [
+    desktops: [
         {
-            "applets": [
-            ],
-            "config": {
+            applets: [],
+            config: {
                 "/": {
-                    "formfactor": "0",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
+                    formfactor: "0",
+                    immutability: "1",
+                    lastScreen: "0",
+                    wallpaperplugin: "org.kde.image",
                 },
                 "/ConfigDialog": {
-                    "DialogHeight": "540",
-                    "DialogWidth": "720"
+                    DialogHeight: "540",
+                    DialogWidth: "720",
                 },
                 "/Configuration": {
-                    "PreloadWeight": "10"
+                    PreloadWeight: "10",
                 },
                 "/General": {
-                    "ToolBoxButtonState": "topcenter",
-                    "ToolBoxButtonX": "462",
-                    "ToolBoxButtonY": "32"
+                    ToolBoxButtonState: "topcenter",
+                    ToolBoxButtonX: "462",
+                    ToolBoxButtonY: "32",
                 },
                 "/Wallpaper/org.kde.image/General": {
-                    "Image": "file:///usr/share/wallpapers/DarkestHour/contents/images/1920x1080.jpg"
-                }
+                    Image:
+                        "file:///usr/share/wallpapers/DarkestHour/contents/images/1920x1080.jpg",
+                },
             },
-            "wallpaperPlugin": "org.kde.image"
-        }
+            wallpaperPlugin: "org.kde.image",
+        },
     ],
-    "panels": [
+    panels: [
         {
-            "alignment": "left",
-            "applets": [
+            alignment: "left",
+            applets: [
                 {
-                    "config": {
+                    config: {
                         "/": {
-                            "immutability": "1"
+                            immutability: "1",
                         },
                         "/Configuration": {
-                            "PreloadWeight": "100"
+                            PreloadWeight: "100",
                         },
                         "/Configuration/General": {
-                            "favoritesPortedToKAstats": "true",
-                            "systemApplications": "systemsettings.desktop,org.kde.kinfocenter.desktop"
+                            favoritesPortedToKAstats: "true",
+                            systemApplications:
+                                "systemsettings.desktop,org.kde.kinfocenter.desktop",
                         },
                         "/Configuration/Shortcuts": {
-                            "global": "Alt+F1"
+                            global: "Alt+F1",
                         },
                         "/Shortcuts": {
-                            "global": "Alt+F1"
-                        }
+                            global: "Alt+F1",
+                        },
                     },
-                    "plugin": "org.kde.plasma.kickoff"
+                    plugin: "org.kde.plasma.kickoff",
                 },
                 {
-                    "config": {
+                    config: {
                         "/": {
-                            "immutability": "1"
+                            immutability: "1",
                         },
                         "/Configuration": {
-                            "PreloadWeight": "10"
-                        }
+                            PreloadWeight: "10",
+                        },
                     },
-                    "plugin": "org.kde.plasma.appmenu"
+                    plugin: "org.kde.plasma.appmenu",
                 },
                 {
-                    "config": {
+                    config: {
                         "/": {
-                            "immutability": "1"
+                            immutability: "1",
                         },
                         "/Configuration": {
-                            "PreloadWeight": "10"
-                        }
+                            PreloadWeight: "10",
+                        },
                     },
-                    "plugin": "org.kde.plasma.panelspacer"
+                    plugin: "org.kde.plasma.panelspacer",
                 },
                 {
-                    "config": {
+                    config: {
                         "/": {
-                            "immutability": "1"
+                            immutability: "1",
                         },
                         "/Configuration": {
-                            "PreloadWeight": "100"
-                        }
+                            PreloadWeight: "100",
+                        },
                     },
-                    "plugin": "org.kde.plasma.systemtray"
+                    plugin: "org.kde.plasma.systemtray",
                 },
                 {
-                    "config": {
+                    config: {
                         "/": {
-                            "immutability": "1"
+                            immutability: "1",
                         },
                         "/Configuration": {
-                            "PreloadWeight": "95"
+                            PreloadWeight: "95",
                         },
                         "/Configuration/Appearance": {
-                            "displayTimezoneAsCode": "false",
-                            "spinboxHorizontalPercentage": "60",
-                            "use24hFormat": "2"
+                            displayTimezoneAsCode: "false",
+                            spinboxHorizontalPercentage: "60",
+                            use24hFormat: "2",
                         },
                         "/Configuration/ConfigDialog": {
-                            "DialogHeight": "540",
-                            "DialogWidth": "720"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.splitdigitalclock"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
+                            DialogHeight: "540",
+                            DialogWidth: "720",
                         },
-                        "/Configuration": {
-                            "PreloadWeight": "50"
-                        }
                     },
-                    "plugin": "org.kde.milou"
-                }
+                    plugin: "org.kde.plasma.splitdigitalclock",
+                },
             ],
-            "config": {
+            config: {
                 "/": {
-                    "formfactor": "2",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
+                    formfactor: "2",
+                    immutability: "1",
+                    lastScreen: "0",
+                    wallpaperplugin: "org.kde.image",
                 },
                 "/ConfigDialog": {
-                    "DialogHeight": "74",
-                    "DialogWidth": "1366"
+                    DialogHeight: "74",
+                    DialogWidth: "1366",
                 },
                 "/Configuration": {
-                    "PreloadWeight": "10"
-                }
+                    PreloadWeight: "10",
+                },
             },
-            "height": 1.7777777777777777,
-            "hiding": "normal",
-            "location": "top",
-            "maximumLength": 75.88888888888889,
-            "minimumLength": 75.88888888888889,
-            "offset": 0
-        }
+            height: 1.7777777777777777,
+            hiding: "normal",
+            location: "top",
+            maximumLength: 100,
+            minimumLength: 100,
+            offset: 0,
+        },
     ],
-    "serializationFormatVersion": "1"
-}
-;
-
+    serializationFormatVersion: "1",
+};
 plasma.loadSerializedLayout(layout);
